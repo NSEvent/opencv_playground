39a6983b06d0e144315f7ef1cbedc4de1d5e18d3
Kevin Tang <kvntng@gmail.com>
2019-08-17 12:14:45 -0400

    Show images in opencv and matplotlib

--- show_image.py
+++ show_image.py
@@ -2,11 +2,23 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Read in image
 img = cv2.imread('pikachu.jpg')
-while True:
-	cv2.imshow('Pikachu!', img)
 
-	if cv2.waitKey(0):# & 0xFF == 27:
-		break
+# Read in grayscale image
+img_grayscale = cv2.imread('pikachu.jpg', cv2.IMREAD_GRAYSCALE)
 
+cv2.imshow('Pikachu!', img)
+cv2.imshow('Pikachu grayscale!', img_grayscale)
+
+cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+# opencv BGR image to matplotlib RGB changes colors
+plt.imshow(img)
+plt.show()
+
+plt.imshow(img_grayscale, cmap='gray', interpolation='bicubic')
+plt.show()
+
