c17503cfb7d6fe06af04ecdd831d7a3fec669ed7
Kevin Tang <kvntng@gmail.com>
2019-08-17 12:48:46 -0400

    Added comments

--- show_image.py
+++ show_image.py
@@ -1,24 +1,31 @@
 import cv2
-import numpy as np
 import matplotlib.pyplot as plt
 
-# Read in image
+# Read in image to cv2 image
 img = cv2.imread('pikachu.jpg')
 
-# Read in grayscale image
+# Read in grayscale image to cv2 image
 img_grayscale = cv2.imread('pikachu.jpg', cv2.IMREAD_GRAYSCALE)
 
+# Show cv2 image
 cv2.imshow('Pikachu!', img)
 cv2.imshow('Pikachu grayscale!', img_grayscale)
 
+# Wait until any key is pressed
 cv2.waitKey(0)
+
+# Close windows
 cv2.destroyAllWindows()
 
+# Write cv2 image to jpg
+cv2.imwrite('pikachu_gray.jpg', img_grayscale);
+
+# open cv2 images with plt
 
 # opencv BGR image to matplotlib RGB changes colors
-plt.imshow(img)
-plt.show()
+# plt.imshow(img)
+# plt.show()
 
-plt.imshow(img_grayscale, cmap='gray', interpolation='bicubic')
-plt.show()
+# plt.imshow(img_grayscale, cmap='gray', interpolation='bicubic')
+# plt.show()
 
